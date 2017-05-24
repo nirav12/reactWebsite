a74ecd5210f3901978a1ca11fa921706e4cf64ce
prshnth <pjreddy.k@gmail.com>
2017-05-23 22:22:11 -0400

    Code for reactApp

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-alert("hi");
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import * as RegFormComponent from "./components/RegFormComponent";
+import * as RegFormReducers from "./reducers/RegFormReducers"; 
+
+const store = createStore(RegFormReducers.regForm);
+const rootEl = document.getElementById("content");
+
+export class App extends React.Component{
+
+	constructor(){
+		super();
+
+	}
+
+	render(){
+
+		return (
+			<Provider store={store}>
+				<RegForm />
+			</Provider>
+
+		);
+
+	}
+
+}
+
+ReactDOM.render(
+	<App />,
+    rootEl
+)
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
+var webpack = require('webpack');
 module.exports = {
-	entry: "/src/index.js",
+	entry: "./src/index.js",
 	output: {
 		path: __dirname + "/dist",
 		filename: "bundle.js"
-	}
+	},
+	resolve: {
+        extensions: ['.tsx', '.ts', '.js','.jsx']
+    }
 }
+
